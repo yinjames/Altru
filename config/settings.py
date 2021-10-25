8223a9a6021cce42985aed8cde41b28da9ce01fe
yinjames <jyin@st.ug.edu.gh>
2021-10-25 18:42:01 +0000

    config updated

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^+3t04k=&1)i93m1erk2rv#5f+8a@_@z#7h!pjt86z#_qr+buo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
