526197b0c6a34106b69a0d4a4682c3f72a7536f4
yinjames <jyin@st.ug.edu.gh>
2021-10-26 10:34:09 +0000

    config updated

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^+3t04k=&1)i93m1erk2rv#5f+8a@_@z#7h!pjt86z#_qr+buo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -99,7 +99,7 @@ ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT = 86400
 ACCOUNT_LOGOUT_REDIRECT_URL = '/'
 LOGIN_REDIRECT_URL = '/'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
-#ACCOUNT_SIGNUP_REDIRECT_URL = "donor/profile/"
+ACCOUNT_SIGNUP_REDIRECT_URL = '/'
 
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
